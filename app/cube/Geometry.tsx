1bff812ef7b8d193b7befa9a07e7663c3cd2f1ef
MagicSakuraD <ckgoforit@outlook.com>
2024-05-21 13:50:43 +0800

    chore: Add @react-three/drei dependency and update cube geometry and lighting

--- app/cube/Geometry.tsx
+++ app/cube/Geometry.tsx
@@ -1,26 +1,39 @@
-import { useFrame } from "@react-three/fiber";
+import { useThree, extend, useFrame } from "@react-three/fiber";
 import React, { useRef } from "react";
 import * as THREE from "three";
+import { OrbitControls } from "@react-three/drei";
 
 const Geometry = () => {
   const torusRef = useRef<THREE.Mesh>(null);
+  const groupRef = useRef<THREE.Group>(null);
+  const { camera, gl } = useThree();
+
   useFrame((state, delta) => {
-    if (torusRef.current) {
+    if (torusRef.current && groupRef.current) {
       torusRef.current.rotation.x += delta;
       torusRef.current.rotation.y += delta;
     }
   });
   return (
-    <group>
-      {/* <mesh scale={[0.5, 0.5, 0.5]} positon-y={-2}>
-    <sphereGeometry args={[3, 32, 32]} />
-    <meshToonMaterial color={"#7c3aed"} />
-  </mesh> */}
-      <mesh ref={torusRef} position={[0, 1, 0]} rotation-x={0.5} scale={0.8}>
-        <torusKnotGeometry />
-        <meshToonMaterial args={[{ color: "#8b5cf6" }]} />
-      </mesh>
-    </group>
+    <>
+      <OrbitControls />
+      <directionalLight
+        position={[1, 2, 3]}
+        color={"#10b981"}
+        intensity={1.5}
+      />
+      <ambientLight intensity={0.5} />
+      <group ref={groupRef}>
+        <mesh scale={[0.5, 0.5, 0.5]} position-y={4}>
+          <sphereGeometry args={[3, 32, 32]} />
+          <meshStandardMaterial color={"#7c3aed"} />
+        </mesh>
+        <mesh ref={torusRef} position={[0, 1, 0]} rotation-x={0.5} scale={0.8}>
+          <torusKnotGeometry />
+          <meshToonMaterial args={[{ color: "#8b5cf6" }]} />
+        </mesh>
+      </group>
+    </>
   );
 };
 
